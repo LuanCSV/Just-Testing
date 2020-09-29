cd20bc05b092b52120cb7765cd8f4ebba5364b53
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-09-29 14:20:19 -0300

    render player

--- Game Básico/script.js
+++ Game Básico/script.js
@@ -1,128 +1,119 @@
 
 var canvas = document.querySelector('#canvas');
-
 var context = canvas.getContext("2d");
 var wCanvas = canvas.clientWidth;
 var hCanvas = canvas.clientHeight;
 
-canvas.addEventListener("mousemove", logMouse)
+document.body.addEventListener("keyup", movimentaPlayer)
 
-function logMouse(e) {
-    const { offsetX, offsetY } = e;
-    console.log(e.offsetX, e.offsetY)
-    interceccaoElementomatrix(offsetX, offsetY)
+var matrizAreaJogo = [];
+
+// Definindo area do topo
+for(let x = 0; x < 10; x++){
+    matrizAreaJogo.push(
+        {
+            y: 0,
+            x: (x * 50),
+            w: 50,
+            h: 50
+        }
+    )
 }
 
-// var matriz = [
-//     [1,1,1,1,1],
-//     [1,0,0,0,1],
-//     [1,0,0,0,1],
-//     [1,1,0,0,1],
-//     [1,1,1,1,1]
-// ]
-
-var matriz = [];
+//Definindo area da esquerda
 for (let x = 0; x < 10; x++) {
-    matriz[x] = Array(10).fill(0)
+    matrizAreaJogo.push(
+        {
+            y: (x * 50),
+            x: 0,
+            w: 50,
+            h: 50
+        }
+    )
+    
 }
 
-function desenhaMatriz() {
-    // randomizarPreenchimento()
+//Definindo area da direita
+for (let x = 0; x < 10; x++) {
+    matrizAreaJogo.push(
+        {
+            y: (x * 50),
+            x: 450,
+            w: 50,
+            h: 50
+        }
+    )
+    
+}
+
+// Definindo area da base
+for(let x = 0; x < 10; x++){
+    matrizAreaJogo.push(
+        {
+            y: 450,
+            x: (x * 50),
+            w: 50,
+            h: 50
+        }
+    )
+}
+
+//Definindo o Player
+const player = {
+    x: 200,
+    y: 200,
+    w: 50, 
+    h: 50
+}
+
+function movimentaPlayer(e) {
+    const teclaPressionada = e.key;
+    const teclasPermitidas = [
+        "ArrowUp",
+        "ArrowLeft",
+        "ArrowDown",
+        "ArrowRight"
+    ];
+
+    if(teclasPermitidas.includes(teclaPressionada)) {
+        console.log(teclaPressionada, "- Ok");
+    }else{
+        console.log("Tecla não permitida: " + teclaPressionada);
+    }
+}
+
+
+function renderizarJogo() {
+    // Reseta o canvas (folha em branco)
     context.clearRect(0, 0, wCanvas, hCanvas)
     
-    for (let x = 0; x < matriz.length; x++) {
-        for (let y = 0; y < matriz[x].length; y++) {
-            context.beginPath();
-            if (matriz[x][y] === 0) {
-                context.fillStyle = "#FFF"    
-            }else{
-                context.fillStyle = "#000"    
-            }
-            context.fillRect(
-                x * (wCanvas / matriz.length), 
-                y * (hCanvas / matriz[x].length),
-                (wCanvas / matriz[x].length),
-                (hCanvas / matriz.length));
-        }   
+    // Desenha área do jogo
+    for (let x = 0; x < matrizAreaJogo.length; x++) {
+       context.beginPath();
+       context.fillStyle = "orange"
+       context.fillRect(
+           matrizAreaJogo[x].x,
+           matrizAreaJogo[x].y,
+           matrizAreaJogo[x].w,
+           matrizAreaJogo[x].h
+           )
     }
-    requestAnimationFrame(desenhaMatriz);
-}
-
-function interceccaoElementomatrix(mouseX, mouseY) {
-    for (let x = 0; x < matriz.length; x++) {
-        for (let y = 0; y < matriz[x].length; y++) {
-            const eleX = x * (wCanvas / matriz.length);
-            const eleY = y * (hCanvas / matriz[x].length);
-            const eleW = (wCanvas / matriz[x].length)
-            const eleH = (hCanvas / matriz.length)
-            
-            if (
-                (mouseX >= eleX && mouseX <= (eleX + eleW))
-                &&
-                (mouseY >= eleY && mouseY <= (eleY + eleH))
-            ) {
-                matriz[x][y] = 1;
-                // context.beginPath();
-                // context.rect(eleX, eleY, eleW, eleH);
-                // context.fill();
-            } else {
-                matriz[x][y] = 0;
-                // context.beginPath();
-                // context.rect(eleX, eleY, eleW, eleH);
-                // context.fillStyle = "#AAA"
-                // context.fill();
-                // context.stroke();
-            }
-        }   
-    }
-}
-
-function randomizarPreenchimento() {
-    for (let x = 0; x < matriz.length; x++) {
-        for (let y = 0; y < matriz[x].length; y++) {
-            matriz[x][y] = Math.round(Math.random())
-            
-        }
-        
-    }
-}
-
-
-function desenha() {
+    // Desenha o player
     context.beginPath();
-    context.moveTo(0,0);
-    context.lineTo(250,250);
-    context.stroke();
+    context.fillStyle = "#F05"
+    context.fillRect(
+        player.x,
+        player.y,
+        player.w,
+        player.h
+    )
+
+    // redesenha o canvas em 60fps
+    requestAnimationFrame(renderizarJogo);
 }
 
-function desenhaQuadrado(){
-
-}
-
-function aleatorio(min, max) {
+function random(min, max) {
     return min + Math.floor((max - min) * Math.random());
 }
 
-// function loop() {
-//     console.log(new Date());
-// }
-
-// // var timeInterval;
-// // var timeTimeout;
-
-// // timeInterval = setInterval(loop, 1000 / 60);
-// // timeTimeout = setTimeout(loop, 5000);
-
-// // setTimeout(()=>{
-// //     clearTimeout(timeTimeout());
-// //     console.log('Passou no timeout de 4000');
-// // }, 6000)
-
-// var animate = requestAnimationFrame(loop);
-
-// function pararLoop() {
-//     clearInterval(timeInterval);
-//     console.log('Parando setInterval');
-// }
-
-desenhaMatriz();
+renderizarJogo();
