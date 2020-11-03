9a4d1b3344416ad2fbd8de3a42a5f3106f3fa295
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-11-03 14:12:45 -0300

    header criado

--- LOL-API/react-v/src/App.js
+++ LOL-API/react-v/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
 
 import './App.css';
+import { detectarDevice } from './Helpers/Device';
 
 function App() {
+
+  const [currentDevice, setCurrentDevice] = useState(detectarDevice());
+
+  useEffect(() => {
+    const updateDevice = () => {
+      setCurrentDevice(detectarDevice())
+    }
+    window.addEventListener('resize', updateDevice);
+    updateDevice();
+    return () => {
+      window.removeEventListener('resize', updateDevice);
+    }
+  }, [currentDevice]);
+
   return (
     <div className="App">
-      <Header/>
+      <Header currentDevice={currentDevice}/>
     </div>
   );
 }
--- LOL-API/react-v/src/components/Header/index.js
+++ LOL-API/react-v/src/components/Header/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import bars from '../../bars-solid.svg';
+import times from '../../times-solid.svg';
 import './style.css';
 
-const Header = () => {
+const Header = ({currentDevice}) => {
+
+  let isMobile = currentDevice === 'mobile';
+  let isTablet = currentDevice === 'tablet';
+  
+  const [activeMenu, setActiveMenu] = useState(false);
+
+  const toggleActiveMenu = () => {
+    setActiveMenu(!activeMenu);
+  }
+
+  console.log(activeMenu)
+  console.log(currentDevice)
+
+  const searchChampions = () => {}
+
   return (
-    <header className='header'>
-      <nav>
+    <header className={`header ${currentDevice}`}>
+      <img 
+        src={activeMenu ? times : bars} 
+        alt={`${!activeMenu? 'Abrir Menu': 'Fechar Menu'}`}
+        className={`header-bars ${currentDevice} ${activeMenu? 'active': ''}`}
+        onClick={toggleActiveMenu}
+      />
+
+      <nav className='header-title'>
         <img src="https://raw.githubusercontent.com/mmtrt/leagueoflegends/master/snap/gui/leagueoflegends.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy"/>
         <h1>League of Legends - API</h1>
+      </nav>
+      <nav className={`header-search ${activeMenu && (isMobile || isTablet) ? 'active-menu': ''}`}>
+        <input type="search" className="searchBar"/>
+        <button className="searchButton" onClick={() => { searchChampions() }}>Search</button>
       </nav>
     </header>
   )
